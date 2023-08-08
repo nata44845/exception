945c12d6c702510124a60c2cb36d8e6ea9c4d2da
nata44845 <krppost@yandex.ru>
2023-08-08 13:59:41 +0700

    homework2

--- homework2/Task3/task3.java
+++ homework2/Task3/task3.java
@@ -1,5 +1,26 @@
 package Task3;
 
+import java.io.FileNotFoundException;
+
 public class task3 {
-    
+    public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 0;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
 }
